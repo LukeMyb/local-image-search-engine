cf5f3676aede1c7252b11fd270432253b19fa697
LukeMyb <ryutten@icloud.com>
2026-04-26 02:32:55 +0900

    ディレクトリ名をYYYYMMに変更

--- scripts/sort.py
+++ scripts/sort.py
@@ -7,8 +7,7 @@ from pathlib import Path
 
 # 元のTARGET_DIRを「読み込み元」と「保存先」に分割
 SOURCE_DIR = r"data\images" # 大元のフォルダ
-TARGET_DIR = r"data\sorted"     # 新しく作成してコピーする先のフォルダ
-BATCH_SIZE = 1000  # 1ディレクトリあたりの最大枚数
+TARGET_DIR = r"data\sorted" # 新しく作成してコピーする先のフォルダ
 
 # ファイルのハッシュ値を計算する関数（先頭8文字を使用）
 def get_file_hash(file_path):
@@ -34,6 +33,7 @@ def copy_and_distribute_photos():
 
     total_files = len(files) # 進捗表示のために全ファイル数を事前に取得
     success_count = 0
+    skip_count = 0 # 最終結果で重複スキップ数を表示するため
     start_time = time.time() # ループ開始のタイムスタンプを記録
 
     for i, file_path in enumerate(files):
@@ -55,18 +55,17 @@ def copy_and_distribute_photos():
             eta_str = eta_time.strftime("%H:%M:%S")
 
             print(f"進捗: {current_count} / {total_files} 件 ... 残り時間: 約 {remaining_td} (終了予定: {eta_str})")
+        
+        # 更新日時をタイムスタンプとして取得
+        mtime = file_path.stat().st_mtime
+        dt = datetime.datetime.fromtimestamp(mtime)
 
-        # フォルダ番号の計算
-        folder_num = (i // BATCH_SIZE) + 1
-        sub_dir_name = f"{folder_num:03d}"
+        # dtから年月（YYYYMM）を取得してディレクトリ名に設定
+        sub_dir_name = dt.strftime("%Y%m")
         dest_dir = target_base_path / sub_dir_name
         
         # フォルダがなければ作成
         dest_dir.mkdir(parents=True, exist_ok=True)
-
-        # 更新日時をタイムスタンプとして取得
-        mtime = file_path.stat().st_mtime
-        dt = datetime.datetime.fromtimestamp(mtime)
         
         # ファイル名作成
         file_hash = get_file_hash(file_path) # ハッシュ値を取得してファイル名に組み込む
@@ -84,7 +83,7 @@ def copy_and_distribute_photos():
         except Exception as e:
             print(f"エラー発生 ({file_path.name}): {e}")
 
-    print(f"処理完了: {success_count} 件のファイルをコピーし、{sub_dir_name} までのフォルダに整理しました。")
+    print(f"処理完了: {success_count} 件のファイルを整理し、{skip_count} 件の重複をスキップしました。")
 
 if __name__ == "__main__":
     copy_and_distribute_photos()
